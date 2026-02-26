f7bc8c57f9f5e82bba367d8307516c6ed328675d
miraclesamp <babalaanana5@gmail.com>
2026-02-26 23:30:10 +0700

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5JGQ3eARM5Lo2IG3mkpdx7p3uh0nrBIk",
+  authDomain: "samp-miracle.firebaseapp.com",
+  projectId: "samp-miracle",
+  storageBucket: "samp-miracle.firebasestorage.app",
+  messagingSenderId: "465126706451",
+  appId: "1:465126706451:web:b94f2359a348ff7078dee6",
+  measurementId: "G-ZCRVX5DCPR"
+};
